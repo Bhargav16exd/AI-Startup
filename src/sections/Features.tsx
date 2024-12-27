b7c07f0747036bc589040134a8de77e071d4c3f1
Bhargav Mule <bhargavmule16@gmail.com>
2024-12-27 13:51:20 +0530

    Features section add

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,8 @@
+'use client'
+import { DotLottiePlayer } from "@dotlottie/react-player";
+import ProductImage from "@/assets/product-image.png"
+import Image from "next/image";
+
 const tabs = [
   {
     icon: "/assets/lottie/vroom.lottie",
@@ -26,5 +31,55 @@ const tabs = [
 ];
 
 export const Features = () => {
-  return <section>Features</section>;
+  return <section className="py-20 text-white md:py-24">
+
+    <div className="container ">
+
+      <h2 className="font-medium text-5xl tracking-tighter text-center md:text-6xl">Elevate your SEO efforts</h2>
+
+      <p className="mt-5 text-white/70 text-lg tracking-tight text-center">
+      From small startups to large enterprises, our AI-driven tool has revolutionized the way businesses approach SEO.
+      </p>
+
+      <div className="flex flex-col mt-10 gap-3 md:flex-row md:justify-between">
+        {
+          tabs.map((tab,index)=>(
+
+            <div key={index} className="py-[10px] px-[10px] flex items-center border border-white/20 rounded-xl gap-[10px] md:flex-1">
+              
+              <div className="h-12 w-12  border border-white/20 rounded-lg flex justify-center items-center">
+               <DotLottiePlayer src={tab.icon} className="h-5 w-5" autoplay />
+              </div>
+              
+
+              <div className="font-medium tracking-tighter">
+              {tab.title}
+              </div>
+
+              {
+                tab.isNew && (
+                  <div className="text-xs font-semibold rounded-[99px] text-black bg-[#8C44FF] py-0.5 px-2">
+                    new
+                  </div>
+                )
+              }
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="border border-white/20 rounded-xl py-2.5 px-2.5 mt-3">
+        
+        <div
+        className="border border-white/20 rounded-lg aspect-video bg-cover"
+        style={{
+          backgroundImage:`url(${ProductImage.src})`
+        }}
+        ></div>
+
+      </div>
+
+    </div>
+
+  </section>;
 };
