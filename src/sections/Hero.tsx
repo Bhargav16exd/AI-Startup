fbe52fed3ccbbc62dfff0589e8762896db917730
Bhargav Mule <bhargavmule16@gmail.com>
2024-12-31 10:20:25 +0530

    Hero and logo ticker animation add

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,34 @@
+'use client'
 import { Button } from "@/components/Button";
 import StarsBg from "@/assets/stars.png"
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const Hero = () => {
-  return <section 
+
+
+  const sectionRef = useRef(null)
+
+  const {scrollYProgress} = useScroll({
+    target:sectionRef,
+    offset:["start end" , "end start"]
+  })
+
+  const YProgress = useTransform(scrollYProgress,[0,1],[-300,300])
+
+  return <motion.section 
+  ref={sectionRef}
   style={{
-    backgroundImage:`url(${StarsBg.src})`
+    backgroundImage:`url(${StarsBg.src})`,
+    backgroundPositionY:YProgress
+  }}
+  animate={{
+    backgroundPositionX : StarsBg.width
+  }}
+  transition={{
+    repeat:Infinity,
+    ease:"linear",
+    duration:60
   }}
   className="text-white h-[492px] flex items-center relative overflow-hidden [mask-image:linear-gradient(to_bottom,transparent,black_20%,black_80%,transparent)] md:h-[800px]" >
 
@@ -14,19 +38,70 @@ export const Hero = () => {
   <div className="absolute h-64 w-64 md:h-96 md:w-96 rounded-full border bg-purple-500 border-white/10  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))]  shadow-[-20px_-20px_50px_rgb(255,255,255,.5),-20px_-20px_80px_rgb(255,255,255,.1),0_0_50px_rgb(140,69,255)] ] "></div>
 
   {/* Planet Ring 1 */}
-  <div className="absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] border rounded-full opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+  <motion.div 
+  style={{
+    translateX:"-50%",
+    translateY:"-50%"
+  }}
+  animate={{
+    rotate:"1turn"
+  }}
+  transition={{
+    repeat:Infinity,
+    repeatType:"loop",
+    ease:"linear",
+    duration:30
+    
+  }}
+  className="absolute h-[344px] w-[344px] md:h-[580px] md:w-[580px] border rounded-full opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
     <div className="h-2 w-2 absolute bg-white rounded-full top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 "></div>
     <div className="h-2 w-2 absolute bg-white rounded-full top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
     <div className="h-5 w-5 absolute border rounded-full top-1/2 left-full -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
      <div className="h-2 w-2 absolute bg-white rounded-full "></div>
     </div>
-  </div>
+  </motion.div>
 
   {/* Planet Ring 2 */}
-  <div className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] border rounded-full border-dashed opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+  <motion.div 
+   style={{
+    translateX:"-50%",
+    translateY:"-50%"
+  }}
+  animate={{
+    rotate:"-1turn"
+  }}
+  transition={{
+    repeat:Infinity,
+    repeatType:"loop",
+    ease:"linear",
+    duration:70
+    
+  }}
+  className="absolute h-[444px] w-[444px] md:h-[780px] md:w-[780px] border rounded-full border-dashed opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></motion.div>
 
   {/* Planet Ring 3 */}
-  <div className="absolute h-[544px] w-[544px] border rounded-full md:h-[980px] md:w-[980px]  opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
+  <motion.div 
+   style={{
+    translateX:"-50%",
+    translateY:"-50%"
+  }}
+  animate={{
+    rotate:"1turn"
+  }}
+  transition={{
+    repeat:Infinity,
+    repeatType:"loop",
+    ease:"linear",
+    duration:50
+    
+  }}
+  className="absolute h-[544px] w-[544px] border rounded-full md:h-[980px] md:w-[980px]  opacity-20  top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="h-2 w-2 absolute bg-white rounded-full top-1/2 left-0 -translate-x-1/2 -translate-y-1/2 "></div>
+    <div className="h-2 w-2 absolute bg-white rounded-full top-0 left-1/2 -translate-x-1/2 -translate-y-1/2 "></div>
+    <div className="h-5 w-5 absolute border rounded-full top-1/2 left-full -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+     <div className="h-2 w-2 absolute bg-white rounded-full "></div>
+    </div>
+  </motion.div>
 
 
 
@@ -48,5 +123,5 @@ export const Hero = () => {
 
   </div>
 
-  </section>;
+  </motion.section>;
 };
--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,9 +1,11 @@
+'use client'
 import AcmeLogo from "@/assets/logo-acme.png"
 import PulseLogo from "@/assets/logo-pulse.png"
 import EchoLogo from "@/assets/logo-echo.png"
 import CelestialLogo from "@/assets/logo-celestial.png"
 import ApexLogo from "@/assets/logo-apex.png"
 import Image from "next/image"
+import { motion } from "framer-motion"
 
 export const LogoTicker = () => {
   return <section className="py-20 text-white overflow-x-clip md:py-24">
@@ -15,8 +17,20 @@ export const LogoTicker = () => {
          <h2>Trusted by top innovative teams</h2>
         </div>
        
-        <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)] ">
-          <div className="flex flex-none gap-14">
+        <div className="flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
+
+          <motion.div 
+          animate={{
+            translateX:'-50%'
+          }}
+          transition={{
+            repeat:Infinity,
+            repeatType:"loop",
+            ease:"linear",
+            duration:10
+          }}
+          className="flex flex-none gap-14 pr-14">
+
 
             <Image src={AcmeLogo} alt="Acme logo" className="h-6 w-auto" />
             <Image src={PulseLogo} alt="Pulse logo" className="h-6 w-auto"/>
@@ -24,7 +38,13 @@ export const LogoTicker = () => {
             <Image src={CelestialLogo} alt="Celestial logo" className="h-6 w-auto"/>
             <Image src={ApexLogo} alt="Apex logo" className="h-6 w-auto"/>
 
-          </div>
+            <Image src={AcmeLogo} alt="Acme logo" className="h-6 w-auto" />
+            <Image src={PulseLogo} alt="Pulse logo" className="h-6 w-auto"/>
+            <Image src={EchoLogo} alt="Echo logo" className="h-6 w-auto"/>
+            <Image src={CelestialLogo} alt="Celestial logo" className="h-6 w-auto"/>
+            <Image src={ApexLogo} alt="Apex logo" className="h-6 w-auto"/>
+
+          </motion.div>
         </div>
       </div>
 
